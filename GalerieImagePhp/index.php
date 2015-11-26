9beb804caac0807765c0db153222a0722e2373d3
CharlieLaplante <alexylaplante@hotmail.com>
2015-11-26 12:01:35 -0500

    Fuck a duck

--- GalerieImagePhp/index.php
+++ GalerieImagePhp/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <?php
+include_once("gestimage.php");
 session_start();
 if(isset($SESSION['LoggedIn']))
 {
@@ -13,7 +14,7 @@ if(isset($_POST['GestionImageSubmit']))
         $extension = pathinfo($_FILES["ImageToUpload"]["name"], PATHINFO_EXTENSION);
         if($extension == "jpg" || $extension == "png" || $extension == "gif" || $extension == "jpeg" ||  $extension == "JPG" || $extension == "PNG" || $extension == "GIF" || $extension == "JPEG")
         {
-            $uploadfile = "Image/".uniqid("DepotImage").".".$extension;
+            $uploadfile = "Image/".$SESSION['LoggedIn'].uniqid("DepotImage").".".$extension;
             if(move_uploaded_file($_FILES['ImageToUpload']['tmp_name'], $uploadfile))
             {
                 header("Location: Index.php");
@@ -24,7 +25,7 @@ if(isset($_POST['GestionImageSubmit']))
 
 $fichiers = scandir("image");
 
-echo "<form action='Gestimage.php' method='POST' enctype='multipart/form-data' >";
+echo "<form action='gestimage.php' method='POST' enctype='multipart/form-data' >";
 if ($fichiers !== FALSE) {
     // on parcourt le tableau
     for ($i = 0; $i < sizeof($fichiers); $i++) {
@@ -32,6 +33,7 @@ if ($fichiers !== FALSE) {
         // on évite l'affichage des fichiers cachés
         if ($nomFichier[0] != ".") {
             echo "<button name='ImageClicker' value='image/$nomFichier' type='submit'><img style='max-height:150px; max-width: 200px; height:auto; width:auto; display:block;' src='image/$nomFichier'></button>";
+            echo $SESSION['LoggedIn'];
         }
     }
 } else {

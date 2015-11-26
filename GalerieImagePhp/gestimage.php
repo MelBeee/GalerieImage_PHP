385cc14d75c25ab3d4f68ce3af75e73536df69de
CharlieLaplante <alexylaplante@hotmail.com>
2015-11-26 16:54:57 -0500

    Deasshda

--- GalerieImagePhp/gestimage.php
+++ GalerieImagePhp/gestimage.php
@@ -1,28 +1,87 @@
  <?php
-/**
- * Created by PhpStorm.
- * User: 201356187
- * Date: 2015-11-24
- * Time: 10:23
- */
- if(isset($_POST['SupprimerImage'])) {
-     unlink($_POST['ImageSupp']);
-     header("Location: Index.php");
- }
+     if (session_status() == PHP_SESSION_NONE) {
+         session_start();
+     }
+     $Fichier ="Commentaire.txt";
+     $Array = array();
 
-if(isset($_POST['ImageClicker']))
+     function getStringBetween($str,$from,$to)
      {
-         $Image = $_POST['ImageClicker'];
+         $sub = substr($str, strpos($str,$from)+strlen($from),strlen($str));
+         return substr($sub,0,strpos($sub,$to));
+     }
 
-         echo "<img src='$Image' style='max-height:600px; max-width: 800px; height:auto; width:auto; display:block;' >";
+     function ProccessComment()
+     {
 
-    echo "<form method='POST' enctype='multipart/form-data'>";
-      if(substr_count($Image,$_SESSION['LoggedIn']) == 1 || $_SESSION['LoggedIn']=="admin")
-               {
-                   echo "<input type='submit' value='Supprimer' name='SupprimerImage'>
-           <input type='hidden' name='ImageSupp' value='$Image'>";
-       }
-      echo "</form>";
-  echo "<a href='Index.php'>RETOURNER A LA PAGE DE DEBUT</a>";
+         $handle = fopen("Commentaire.txt", "r");
+         if ($handle)
+         {
+             while (($line = fgets($handle)) !== false)
+             {
+                 if(strpos($line,$_SESSION['ImageCommentaire']) !== false)
+                 {
 
-}
+                     $Array[] = getStringBetween($line,"*","\n");
+                 }
+             }
+
+             fclose($handle);
+         }
+         if(!empty($Array))
+         {
+             for($i = count($Array)-1 ; $i >= 0 ; $i--)
+             {
+                 echo $Array[$i];
+                 echo "<br>";
+             }
+         }
+
+     }
+
+     function gestImageMain()
+     {
+         echo "<img src=".$_SESSION['ImageCommentaire']." style='max-height:600px; max-width: 800px; height:auto; width:auto; display:block;' >";
+
+         echo "<form method='POST' enctype='multipart/form-data'>";
+         if(substr_count($_SESSION['ImageCommentaire'],$_SESSION['LoggedIn']) == 1 || $_SESSION['LoggedIn'] == "admin")
+         {
+             echo "<input type='submit' value='Supprimer' name='SupprimerImage'>
+                                  <input type='hidden' name='ImageSupp' value=",$_SESSION['ImageCommentaire'].">";
+         }
+         echo "</form>";
+
+         echo "<form method='POST' enctype='multipart/form-data'>
+            <input type='text' name='LeCommentaire'>
+            <input type='submit' value='envoyer' name='EnvoyerCommentaire'>";
+
+         echo "<br>";
+
+         ProccessComment();
+
+         echo "<a href='Index.php'>RETOURNER A LA PAGE DE DEBUT</a>";
+     }
+
+    if(isset($_POST['SupprimerImage']))
+    {
+        unlink($_POST['ImageSupp']);
+        header("Location: Index.php");
+    }
+
+    if(isset($_POST['EnvoyerCommentaire']))
+    {
+        if($Handle = fopen($Fichier,'a'))
+        {
+            fwrite($Handle,$_SESSION['ImageCommentaire']."*".$_SESSION['LoggedIn']."_".$_POST['LeCommentaire']."_".date('j-m-y')."\n");
+            gestImageMain();
+        }
+    }
+
+    if(isset($_POST['ImageClicker']))
+    {
+        $_SESSION['ImageCommentaire'] = $_POST['ImageClicker'];
+        gestImageMain();
+    }
+
+
+
--- GalerieImagePhp/index.php
+++ GalerieImagePhp/index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 <html>
+<head>
+    <style>
+        .HeaderShit
+        {
+            color:grey;
+            font-size:25px;
+        }
+    </style>
+</head>
 <?php
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+
 include_once("gestimage.php");
 if(isset($_SESSION['LoggedIn']))
 {
     $extension="";
+
     if(isset($_POST['GestionImageSubmit']))
     {
         if(isset($_FILES['ImageToUpload']['name']))
@@ -15,10 +26,11 @@ if(isset($_SESSION['LoggedIn']))
             $extension = pathinfo($_FILES["ImageToUpload"]["name"], PATHINFO_EXTENSION);
             if($extension == "jpg" || $extension == "png" || $extension == "gif" || $extension == "jpeg" ||  $extension == "JPG" || $extension == "PNG" || $extension == "GIF" || $extension == "JPEG")
             {
-                $uploadfile = "Image/".$_SESSION['LoggedIn'].uniqid("DepotImage").".".$extension;
+
+                $uploadfile = "image/".uniqid($_SESSION['LoggedIn']."(".$_POST['TitreImage']."_".date('j-m-y')."+").".".$extension;
                 if(move_uploaded_file($_FILES['ImageToUpload']['tmp_name'], $uploadfile))
                 {
-                    header("Location: Index.php");
+                    header("Location: index.php");
                 }
             }
         }
@@ -26,17 +38,44 @@ if(isset($_SESSION['LoggedIn']))
 
     $fichiers = scandir("image");
 
+    echo "<a href='profile.php' class='HeaderShit'>Usager: ".$_SESSION['LoggedIn']."</a>  ";
+    if($_SESSION['LoggedIn'] == "admin")
+    {
+        echo "<a href='admin.php' class='HeaderShit' style>ADMIN_PAGE</a>" ;
+    }
     echo "<form action='gestimage.php' method='POST' enctype='multipart/form-data' >";
+
     if ($fichiers !== FALSE) {
         // on parcourt le tableau
+        echo "<table><tr>";
         for ($i = 0; $i < sizeof($fichiers); $i++) {
+            echo "<td>";
             $nomFichier = $fichiers[$i];
+            $NomUsager = substr($nomFichier, 0 ,strpos($nomFichier,"("));
+            $FileTitre = getStringBetween($nomFichier,"(","_");
+            $Date = getStringBetween($nomFichier,"_","+");
+            $NbCommentaire = 0;
+            if($Commentaire = file_get_contents($Fichier))
+            {
+                $NbCommentaire = substr_count($Commentaire,$nomFichier);
+            }
             // on évite l'affichage des fichiers cachés
             if ($nomFichier[0] != ".") {
+                echo "<div>";
                 echo "<button name='ImageClicker' value='image/$nomFichier' type='submit'><img style='max-height:150px; max-width: 200px; height:auto; width:auto; display:block;' src='image/$nomFichier'></button>";
-                echo $_SESSION['LoggedIn'];
+                echo "<br>";
+                echo "Usager :".$NomUsager;
+                echo "<br>";
+                echo "Titre :".$FileTitre;
+                echo "<br>";
+                echo "Date :". $Date;
+                echo "<br>";
+                echo "Nombre de commentaire :".$NbCommentaire;
+                echo "</div>";
             }
+            echo "</td>";
         }
+        echo "</tr></table>";
     } else {
         die("Erreur: repertoire invalide");
     };
@@ -46,7 +85,9 @@ if(isset($_SESSION['LoggedIn']))
 
 
     echo "<form action='' method='POST' enctype='multipart/form-data'>
-        <label for='ImageToUpload'>Votre image a upload</label>
+        <label for='TitreImage'>Titre de l'image :</label>
+        <input type='text' name='TitreImage' id='TitreImage'>
+        <label for='ImageToUpload'>Votre image a upload :</label>
         <input type='file' name='ImageToUpload' id='ImageToUpload'>
         <input type='submit' name='GestionImageSubmit'>
     </form>
